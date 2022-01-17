bbafd8fc0e4fb1c3ecd42d088cbeeaee17a1c95a
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2022-01-17 15:29:48 +0200

    use kts build

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    id("ru.vyarus.quality").version("4.7.0")
+    id("java-library")
+    id("jacoco")
+    id("maven-publish")
+}
+
+val suitesDir = "src/test/resources/suites/"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("org.testng", "testng", "7.5")
+    implementation("org.json", "json", "20211205")
+    implementation("io.rest-assured", "rest-assured", "4.4.0")
+    implementation("com.github.everit-org.json-schema", "org.everit.json.schema", "1.14.0")
+}
+
+tasks.test {
+    useTestNG {
+        // Add test suites
+        File(projectDir.absolutePath + "/" + suitesDir)
+                .walk()
+                .forEach {
+                    if (it.isFile) {
+                        suites(it)
+                    }
+                }
+
+        testLogging {
+            events("passed", "skipped", "failed")
+            exceptionFormat = TestExceptionFormat.FULL
+            showStandardStreams = true
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("${buildDir}/reports/coverage").get().asFile)
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+quality {
+    checkstyle = true
+    pmd = true
+    codenarc = true
+    spotbugs = true
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestReport)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.mbi"
+            artifactId = "json-validator"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
