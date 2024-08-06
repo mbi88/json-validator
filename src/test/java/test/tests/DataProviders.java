9abae9583aa533a36cdd374bd19a30ef1ca737c0
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2024-08-06 14:39:31 +0300

    fix mocks

--- src/test/java/test/tests/DataProviders.java
+++ src/test/java/test/tests/DataProviders.java
@@ -13,7 +13,7 @@ public class DataProviders {
     static Object[][] getSuccessValidationData() {
         return new Object[][]{
                 {getJsonFromFile("/jsons/success_validation_jo.json")},
-                {get("https://run.mocky.io/v3/dbece8ee-ac02-4a59-a221-5bacec4377cd")},
+                {get("https://api.npoint.io/dfce6fd1a49877114cda")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -24,7 +24,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/invalid_type_jo.json")},
                 {getJsonArrayFromFile("/jsons/invalid_type_ja.json")},
-                {get("https://run.mocky.io/v3/51748208-2063-46fe-ab63-62b44280fd96")},
+                {get("https://api.npoint.io/148d863c8b81b191e779")},
                 {"""
                     {"a":"1","b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -34,7 +34,7 @@ public class DataProviders {
     static Object[][] getNullPropertySuccessData() {
         return new Object[][]{
                 {getJsonFromFile("/jsons/null_property_success_jo.json")},
-                {get("https://run.mocky.io/v3/40073f15-ee9b-458f-b7ba-c4efbe847368")},
+                {get("https://api.npoint.io/4ae374e7e33bc8082920")},
                 {"""
                     {"a":1,"b":"a","c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -45,7 +45,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/null_property_failed_jo.json")},
                 {getJsonArrayFromFile("/jsons/null_property_failed_ja.json")},
-                {get("https://run.mocky.io/v3/14e0d9c0-233d-46ea-a09d-d52971238a42")},
+                {get("https://api.npoint.io/cf4ca946c8dc328f5a7c")},
                 {"""
                     {"a":1,"b":1,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -55,7 +55,7 @@ public class DataProviders {
     static Object[][] getEmptyObjectData() {
         return new Object[][]{
                 {getJsonFromFile("/jsons/empty_object_jo.json")},
-                {get("https://run.mocky.io/v3/31e7d4b7-4c9c-4693-9cea-a654a644e70f")},
+                {get("https://api.npoint.io/e28d43c94b98b15ac9a2")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{"a":1},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -65,7 +65,7 @@ public class DataProviders {
     static Object[][] getEmptyArrayData() {
         return new Object[][]{
                 {getJsonArrayFromFile("/jsons/empty_array_ja.json")},
-                {get("https://run.mocky.io/v3/1555976b-19cc-4e66-83af-b6c8412099a6")},
+                {get("https://api.npoint.io/df5e71a711f0b29b95a3")},
                 {"""
                     [{"a":1,"b":null,"c":"1","d":[1],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}},{"a":1,"b":null,"c":"1","d":[1],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}}]"""}
         };
@@ -75,7 +75,7 @@ public class DataProviders {
     static Object[][] getValidateArrayData() {
         return new Object[][]{
                 {getJsonArrayFromFile("/jsons/validate_array_ja.json")},
-                {get("https://run.mocky.io/v3/8045379e-7e5f-4ac0-9cd6-24c63fd6c147")},
+                {get("https://api.npoint.io/c94a5674e5623fb08da8")},
                 {"""
                     [{"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1}],"g":{"aa":1,"bb":{"aaa": 1}}}, {"a": 2, "b": null, "c": "1", "d": [], "e": {}, "f": [{"aa": 1}], "g": {"aa": 1, "bb": {"aaa": 1}}}]"""}
         };
@@ -86,7 +86,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/extra_field_jo.json")},
                 {getJsonArrayFromFile("/jsons/extra_field_ja.json")},
-                {get("https://run.mocky.io/v3/69edb648-093a-4798-9fd4-2d017f799140")},
+                {get("https://api.npoint.io/b6314d7b65a0dffbe811")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1,"a":1}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -97,7 +97,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/extra_object_jo.json")},
                 {getJsonArrayFromFile("/jsons/extra_object_ja.json")},
-                {get("https://run.mocky.io/v3/10ee30ca-e167-490c-85a9-4df783a2fe1d")},
+                {get("https://api.npoint.io/2c00dc55fd99a4b28ce4")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"bb":1}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -108,7 +108,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/wo_field_jo.json")},
                 {getJsonArrayFromFile("/jsons/wo_field_ja.json")},
-                {get("https://run.mocky.io/v3/21671812-a4ce-42ef-b773-b881853e69bb")},
+                {get("https://api.npoint.io/76b78c57196508860f39")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"bb":1}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -119,7 +119,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/invalid_value_jo.json")},
                 {getJsonArrayFromFile("/jsons/invalid_value_ja.json")},
-                {get("https://run.mocky.io/v3/7ba00948-23d6-4baf-83ea-2f478094b803")},
+                {get("https://api.npoint.io/9c75691cc117e1d4da52")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":"1"}],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -130,7 +130,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/empty_array_failed_jo.json")},
                 {getJsonArrayFromFile("/jsons/empty_array_failed_ja.json")},
-                {get("https://run.mocky.io/v3/34b67a35-7191-4c07-8f35-d41ff07025e5")},
+                {get("https://api.npoint.io/4a493f95bfef325869cb")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[],"g":{"aa":1,"bb":{"aaa":1}}}"""}
         };
@@ -141,7 +141,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/empty_object_failed_jo.json")},
                 {getJsonArrayFromFile("/jsons/empty_object_failed_ja.json")},
-                {get("https://run.mocky.io/v3/3b325b1b-c8ff-4e16-8df5-6cf1c1f23222")},
+                {get("https://api.npoint.io/c9ee90cf6e6d900e6f52")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{}}"""}
         };
@@ -152,7 +152,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/extra_field_in_nested_object_jo.json")},
                 {getJsonArrayFromFile("/jsons/extra_field_in_nested_object_ja.json")},
-                {get("https://run.mocky.io/v3/99c91dfd-ccf6-4d43-aeb4-7008f6af2b19")},
+                {get("https://api.npoint.io/0761cfcc99b600b49d8e")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1},"ccc":[]}}"""}
         };
@@ -163,7 +163,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/object_without_field_jo.json")},
                 {getJsonArrayFromFile("/jsons/object_without_field_ja.json")},
-                {get("https://run.mocky.io/v3/c41f7406-7bb7-4e46-8934-bbec96c1f23a")},
+                {get("https://api.npoint.io/fc5a11929ca6706c4899")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"bb":{"aaa":1}}}"""}
         };
@@ -174,7 +174,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/object_with_invalid_type_jo.json")},
                 {getJsonArrayFromFile("/jsons/object_with_invalid_type_ja.json")},
-                {get("https://run.mocky.io/v3/f0d208e2-3641-482f-bbaa-1575a934c338")},
+                {get("https://api.npoint.io/09eaaf3eb24aa2fedecd")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":"string","bb":{"aaa":1}}}"""}
         };
@@ -185,7 +185,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/object_without_nested_object_jo.json")},
                 {getJsonArrayFromFile("/jsons/object_without_nested_object_ja.json")},
-                {get("https://run.mocky.io/v3/38fa9b62-1d97-49e7-b2a2-acf0a033451e")},
+                {get("https://api.npoint.io/9cf060c5697ec601dd22")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{}}}"""},
         };
@@ -196,7 +196,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/nested_object_extra_field_failed_jo.json")},
                 {getJsonArrayFromFile("/jsons/nested_object_extra_field_failed_ja.json")},
-                {get("https://run.mocky.io/v3/f0a98504-9bf6-4d4b-a665-1b0fb0d1482a")},
+                {get("https://api.npoint.io/c6d1d182c2ce886ee674")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1,"bbb":"asd"}}}"""}
         };
@@ -207,7 +207,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/nested_object_without_field_failed_jo.json")},
                 {getJsonArrayFromFile("/jsons/nested_object_without_field_failed_ja.json")},
-                {get("https://run.mocky.io/v3/c6c02350-0582-4471-aa2e-3e76e472eae9")},
+                {get("https://api.npoint.io/d373c5bfc754378555ed")},
                 {"""
                     {"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"bbb":"asd"}}}"""}
         };
@@ -218,7 +218,7 @@ public class DataProviders {
         return new Object[][]{
                 {getJsonFromFile("/jsons/array_has_invalid_object_jo.json")},
                 {getJsonArrayFromFile("/jsons/array_has_invalid_object_ja.json")},
-                {get("https://run.mocky.io/v3/cfe05595-613b-412a-ba67-b1bed4fa90e2")},
+                {get("https://api.npoint.io/9b7c94e76e0991a42d1b")},
                 {"""
                     [{"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"aa":2}],"g":{"aa":1,"bb":{"aaa":1}}},{"a":1,"b":null,"c":"1","d":[],"e":{},"f":[{"aa":1},{"bb":2}],"g":{"aa":1,"bb":{"aaa":1}}}]"""}
         };
@@ -227,7 +227,7 @@ public class DataProviders {
     @DataProvider(name = "InvalidJson")
     static Object[][] getInvalidJson() {
         return new Object[][]{
-                {get("https://run.mocky.io/v3/6ac6631e-093e-47e6-87fe-4609b587b927")},
+                {get("https://api.npoint.io/99db55b939e25b519d4d")},
                 {"123"}
         };
     }
